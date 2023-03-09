72604d0430e85d56c8d131706f1cb3e5e6ef4bae
Kamaleshen S <kamaleshen.s@gmail.com>
2023-03-09 21:27:41 +0530

    feat: add UI to create content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Error from './pages/error';
+import PageNotFound from './pages/pageNotFound';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
-      APP
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Login />} />
+          <Route path={'/home'} element={<Home />} />
+          <Route path={`${'/error'}/:errorCode?`} element={<Error />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+export default function Header() {
+
+  return (
+    <div className="header-wrapper">
+      <div className="cms">
+        CMS+
+      </div>
+      <div className="content-type-text">
+        Content Types
+      </div>
+    </div>
+  );
+}
